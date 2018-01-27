f6f3b0858de14226bb47256db4aeaa5290732e60
Samuel Han <shambuhallin334@gmail.com>
2018-01-27 11:14:30 -0500

    Start screen done

--- Doggy Dog World/ViewController.swift
+++ Doggy Dog World/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setBackground()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setBackground() {
+        let background = UIImage(named: "cover2.png")
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIViewContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        //imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubview(toBack: imageView)
     }
 
     override func didReceiveMemoryWarning() {
