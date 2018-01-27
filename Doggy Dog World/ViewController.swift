51d7466e71fc115072959ee0c9e074a6c9d131dd
Samuel Han <shambuhallin334@gmail.com>
2018-01-27 13:12:11 -0500

    Start screen done

--- Doggy Dog World/ViewController.swift
+++ Doggy Dog World/ViewController.swift
@@ -14,16 +14,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setBackground()
+        
+        let registerButton = UIButton(frame: CGRect(x: 100, y: 500, width: 200, height: 50))
+        registerButton.titleLabel?.font = UIFont(name: "TimesNewRomanPSMT", size: 27.0)
+        registerButton.setTitle("Register", for: .normal)
+        registerButton.setTitleColor(UIColor.blue, for: .normal)
+        //instructbutton.addTarget(self, action: #selector(instructionsPressed), for: .touchUpInside)
+        view.addSubview(registerButton)
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = UIButton(frame: CGRect(x: 100, y: 550, width: 200, height: 50))
+        loginButton.titleLabel?.font = UIFont(name: "TimesNewRomanPSMT", size: 27.0)
+        loginButton.setTitle("Log In", for: .normal)
+        loginButton.setTitleColor(UIColor.blue, for: .normal)
+       // loginButton.addTarget(self, action: #selector(instructionsPressed), for: .touchUpInside)
+        view.addSubview(loginButton)
+        
     }
     
+    
+    
     func setBackground() {
-        let background = UIImage(named: "cover2.png")
-        var imageView : UIImageView!
+        let backgroundImage = UIImage(named: "cover2.png")
+        var imageView: UIImageView!
         imageView = UIImageView(frame: view.bounds)
         imageView.contentMode =  UIViewContentMode.scaleAspectFill
         imageView.clipsToBounds = true
-        imageView.image = background
+        imageView.image = backgroundImage
         //imageView.center = view.center
         view.addSubview(imageView)
         self.view.sendSubview(toBack: imageView)
