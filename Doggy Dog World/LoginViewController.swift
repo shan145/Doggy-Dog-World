adec919b0aff8e8318abd11e61cf33e422ec2df7
Joseph Lee <Josephlee@dhcp-hol-991.eduroam.cornell.edu>
2018-01-27 17:14:04 -0500

    progres

--- Doggy Dog World/LoginViewController.swift
+++ Doggy Dog World/LoginViewController.swift
@@ -14,11 +14,11 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userbox: UITextField!
     @IBOutlet weak var password: UILabel!
     @IBOutlet weak var passbox: UITextField!
-    @IBOutlet weak var enter: UIButton!
     @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
 
@@ -27,15 +27,5 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
