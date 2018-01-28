ec2543c65fd48909a8c60e3dd7c0a60d9806819d
Suin Jung <suinjung@dhcp-hol-1534.eduroam.cornell.edu>
2018-01-27 19:30:20 -0500

    hai

---
+++ Doggy Dog World/TaskViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TaskViewController.swift
+//  Doggy Dog World
+//
+//  Created by Suin Jung on 1/27/18.
+//  Copyright © 2018 Samuel Han. All rights reserved.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
